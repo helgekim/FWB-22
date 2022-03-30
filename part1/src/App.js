7faf5884cf10be65f0993a1639ff14f9dc4b5eaf
Helge Kim <met.oleg@pm.me>
2022-03-30 22:25:26 +0500

    part1: 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -47,27 +47,29 @@ function Part({part}) {
 
 function App() {
 
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header name={course}/>
-      <Content content={parts}/>
-      <Total content={parts}/>
+      <Header name={course.name}/>
+      <Content content={course.parts}/>
+      <Total content={course.parts}/>
     </div>
   );
 }
