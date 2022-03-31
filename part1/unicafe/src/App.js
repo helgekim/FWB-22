d3c2e965bec4638df8ebc5e0073fd266c8b83a00
Helge Kim <met.oleg@pm.me>
2022-03-31 13:27:10 +0500

    part1: 1.5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+function Rate({setter, points}) {
+
+  const upGood = () => setter({...points, good: points.good + 1});
+  const upNeutral = () => setter({...points, neutral: points.neutral + 1});
+  const upBad = () => setter({...points, bad: points.bad + 1});
+
+
+  return(
+    <div>
+      <h1> give feedback </h1>
+      <div>
+        <button onClick={upGood}>good</button>
+        <button onClick={upNeutral}>neutral</button>
+        <button onClick={upBad}>bad</button>
+      </div>
+    </div>
+  )
+}
+
+function ShowRates({points}) {
+
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p> good: {points.good} </p>
+      <p> neutral: {points.neutral} </p>
+      <p> bad: {points.bad} </p>
+    </div>
+  )
+}
+
 
 function App() {
+
+  const [points, setPoints] = useState(
+    {
+      good: 0,
+      neutral: 0,
+      bad: 0
+    }
+  )
+
+
+    console.log(points);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> The application is in development </p>
+      <Rate setter={setPoints} points={points}/>
+      <ShowRates points={points}/>
     </div>
   );
 }
