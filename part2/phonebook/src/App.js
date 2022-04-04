643ad89e3909ea5b24148c931e0e1769d9251931
Helge Kim <met.oleg@pm.me>
2022-04-04 20:46:08 +0500

    part2/phonebook: now can one create a phonebook contact (only names) 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,41 @@
+import {useState} from 'react';
+
 
 function App() {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  function addContact(event) {
+    event.preventDefault();
+    const object = {
+      name: newName
+    };
+    return setPersons(persons.concat(object));
+  }
   return (
     <div>
-      <h1> Phonebook </h1>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+
+      </form>
+      </div>
+      <div>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(contact => <li>{contact.name}</li>)}
+      </ul>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
