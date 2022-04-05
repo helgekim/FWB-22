ab1f3c8ace6b3f237f528e6bb47692134a812f0c
Helge Kim <met.oleg@pm.me>
2022-04-05 19:33:13 +0500

    part2/phonebook: 2.10. all related to phonebook code extracted into a separate module (2.10). i will remake it soon

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,71 +1,25 @@
 import {useState} from 'react';
-
+import Book from './components/phonebook'
 
 function App() {
 
-
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+      { name: 'Arto Hellas', number: '040-123456', id: 1 },
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }]
+  );
+
 
   const [search, setSearch] = useState('');
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-
-  const showAll = search === "" ? persons : persons.filter(element => element.name.match(search));
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
 
-  function addContact(event) {
-    event.preventDefault();
-
-    const check = persons.filter(contact => contact.name === newName).length;
-
-    if (check > 0) {
-      return alert(`${newName} cannot be created for it's been already in the contactbook!`);
-    }
-
-
-    if (newName.length >= 3)
-
-    {
-      const object = {
-      name: newName,
-      number: newNumber
-    };
-    return setPersons(persons.concat(object));
-  }
-  else {
-    return alert(`A contact named ${newName} cannot be created`);
-  }
-
-  }
   return (
     <div>
-    <div>
-      <h2>Phonebook</h2>
-      <p> filter shown with <input value={search} onChange={(event) => setSearch(event.target.value)}/></p>
-      <form onSubmit={addContact}>
-        <div>
-          name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-
-      </form>
-      </div>
-      <div>
-      <h2>Numbers</h2>
-      <ul>
-        {showAll.map(contact => <li key={contact.name}>{contact.name}: {contact.number}</li>)}
-      </ul>
-      </div>
+      <Book persons={persons} setPersons={setPersons} search={search} setSearch={setSearch} newName={name}
+            newNumber={number} setNewNumber={setNumber}/>
     </div>
   )
 }
---
+++ part2/phonebook/src/components/phonebook.js
@@ -0,0 +1,61 @@
+
+  function addContact(event) {
+      event.preventDefault();
+
+      const check = persons.filter(contact => contact.name === newName).length;
+
+      if (check > 0) {
+        return alert(`${newName} cannot be created for it's been already in the contactbook!`);
+      }
+
+
+      if (newName.length >= 3)
+
+      {
+        const object = {
+        name: newName,
+        number: newNumber
+      };
+      return setPersons(persons.concat(object));
+    }
+    else {
+      return alert(`A contact named ${newName} cannot be created`);
+  }
+  }
+
+
+
+function Book({persons, setPersons, search, setSearch, newName, newNumber, setNewName, setNewNumber}) {
+
+
+  const showAll = search === "" ? persons : persons.filter(element => element.name.match(search));
+
+
+  return(
+    <div>
+    <div>
+      <h2>Phonebook</h2>
+      <p> filter shown with <input value={search} onChange={(event) => setSearch(event.target.value)}/></p>
+      <form onSubmit={addContact}>
+        <div>
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      </div>
+      <div>
+      <h2>Numbers</h2>
+      <ul>
+        {showAll.map(contact => <li key={contact.name}>{contact.name}: {contact.number}</li>)}
+      </ul>
+    </div>
+    </div>
+  )
+
+}
+export default Book;
